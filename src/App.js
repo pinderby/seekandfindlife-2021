8fc2d23cde1121e53c73a74eb3fc3f543d999ee0
David Mangold <pinderby@gmail.com>
2021-05-08 18:58:15 -0700

    Home page, basic data infrastructure set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import logo from './SFL Logo Transparent.png';
 import './App.css';
+import config from './firebase-config';
+import Home from './Home';
 // From https://www.npmjs.com/package/react-firebaseui
 import firebase from 'firebase';
+import { FirestoreProvider } from "@react-firebase/firestore";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import {
   BrowserRouter as Router,
@@ -11,18 +14,11 @@ import {
   Link
 } from "react-router-dom";
 
-// Configure Firebase.
-const config = {
-  apiKey: "AIzaSyBrDn6coasn_PKyLnTqQ222G8iJ6MIVC3w",
-  authDomain: "seekandfind-life.firebaseapp.com",
-  projectId: "seekandfind-life",
-  storageBucket: "seekandfind-life.appspot.com",
-  messagingSenderId: "1058318644602",
-  appId: "1:1058318644602:web:700bbc505407d23f33d2c3",
-  measurementId: "G-EVJSDY4F7F"
-  // ...
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+} else {
+  firebase.app(); // if already initialized, use that one
 };
-firebase.initializeApp(config);
 
 // Configure FirebaseUI.
 const uiConfig = {
@@ -40,41 +36,31 @@ const uiConfig = {
 function App() {
   
   return (
-    <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/signedIn">
-            <h1>My App</h1>
-            {/* <Home /> */}
-          </Route>
-          <Route path="/">
-            <div className="App">
-              <header className="App-header">
-              <h1>My App</h1>
-              <p>Please sign-in:</p>
-              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
-            {/* <Splash /> */}
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <FirestoreProvider {...config} firebase={firebase}>
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/signedIn">
+              {/* <h1>My App</h1> */}
+              <Home />
+            </Route>
+            <Route path="/">
+              <div className="App">
+                <header className="App-header">
+                <h1>Welcome to SeekandFind.Life!</h1>
+                <img src={logo} alt="logo" style={{width: "300px"}}/>
+                <p>Please sign-in:</p>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+                </header>
+              </div>
+              {/* <Splash /> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </FirestoreProvider>
   );
 }
 
